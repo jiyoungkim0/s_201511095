62625dda41cd5daf29a585dcc8e4ee61ed8836d4
s_201511095 <wldud2424@naver.com>
2017-04-05 19:50:55 +0900

    Add files via upload

---
+++ src/ds_web_crawl_python_url.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import re 
+
+from urllib import FancyURLopener 
+class MyOpener(FancyURLopener): 
+    version = 'My User-Agent(1)' 
+print MyOpener.version 
+
+class MyOpener(FancyURLopener): 
+    version = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0' 
+print MyOpener.version 
+ 
+myopener = MyOpener() 
+page = myopener.open('http://www.google.com/search?q=python') 
+html=page.read() 
+ 
+p=re.compile('href="(https://.*?)"') 
+#p=re.compile('.*href.*') 
+res=p.findall(html) 
+print len(res) 
+for item in res: 
+    print item[:100] 
+
