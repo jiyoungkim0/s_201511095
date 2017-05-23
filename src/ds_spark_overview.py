4197ca18f57fd85575d2ebf48529f4186c2cfc32
s_201511095 <wldud2424@naver.com>
2017-05-23 16:02:11 +0900

    Update ds_spark_overview.py

--- src/ds_spark_overview.py
+++ src/ds_spark_overview.py
@@ -7,7 +7,7 @@ import os
 import sys
 import matplotlib.pyplot as plt 
 
-os.environ["SPARK_HOME"]="C:\Users\ramy\Downloads\spark-2.0.0-bin-hadoop2.6"
+os.environ["SPARK_HOME"]="C:\\Users\\ramy\\Downloads\\spark-2.0.0-bin-hadoop2.6"
 os.environ["PYLIB"]=os.path.join(os.environ["SPARK_HOME"],'python','lib')
 sys.path.insert(0,os.path.join(os.environ["PYLIB"],'py4j-0.10.1-src.zip'))
 sys.path.insert(0,os.path.join(os.environ["PYLIB"],'pyspark.zip'))
@@ -18,7 +18,7 @@ sys.path.insert(0,os.path.join(os.environ["PYLIB"],'pyspark.zip'))
 import pyspark
 
 myConf = pyspark.SparkConf() 
-spark = pyspark.sql.SparkSession.builder.master('local').appName('myApp').config(conf=myConf).config('spark.sql.warehouse.dir','file:///C:\Users\ramy\SMU\BigData\Code\s-201511095\data').getOrCreate() 
+spark = pyspark.sql.SparkSession.builder.master('local').appName('myApp').config(conf=myConf).config('spark.sql.warehouse.dir','file:///C:\\Users\\ramy\\SMU\\BigData\\Code\\s-201511095\\data').getOrCreate() 
 filepath = os.path.join('data','overview.txt')
 myRdd = spark.sparkContext.textFile(filepath)
 wc2 = myRdd.flatMap(lambda x:x.split()).map(lambda x:(x,1)).reduceByKey(lambda x,y:x+y).map(lambda x:(x[1],x[0])).sortByKey(False).take(30) 
